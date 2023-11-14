8ed212c44900b0ec18a2d509a1e5dca1eb311db4
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-11-14 21:00:25 +0600

    firebase added

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,81 +1,87 @@
-import React, { Component } from 'react';
-import Burger from './Burger/Burger';
-import Controls from './Controls/Controls';
-import Summary from './Summary/Summary';
-import { Modal, ModalBody, ModalHeader, ModalFooter, Button } from 'reactstrap';
+import React, { Component } from "react";
+import Burger from "./Burger/Burger";
+import Controls from "./Controls/Controls";
+import Summary from "./Summary/Summary";
+import { Modal, ModalBody, ModalHeader, ModalFooter, Button } from "reactstrap";
 
-import { connect } from 'react-redux';
-import { addIngredient, removeIngredient, updatePurchasable } from '../../redux/actionCreators';
+import { connect } from "react-redux";
+import {
+  addIngredient,
+  removeIngredient,
+  updatePurchasable,
+} from "../../redux/actionCreators";
+import { Link } from "react-router-dom";
 
-const mapStateToProps = state => {
-    return {
-        ingredients: state.ingredients,
-        totalPrice: state.totalPrice,
-        purchasable: state.purchasable,
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    ingredients: state.ingredients,
+    totalPrice: state.totalPrice,
+    purchasable: state.purchasable,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        addIngredient: (igtype) => dispatch(addIngredient(igtype)),
-        removeIngredient: (igtype) => dispatch(removeIngredient(igtype)),
-        updatePurchasable: () => dispatch(updatePurchasable()),
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addIngredient: (igtype) => dispatch(addIngredient(igtype)),
+    removeIngredient: (igtype) => dispatch(removeIngredient(igtype)),
+    updatePurchasable: () => dispatch(updatePurchasable()),
+  };
+};
 
 class BurgerBuilder extends Component {
-    state = {
-        modalOpen: false,
-    }
+  state = {
+    modalOpen: false,
+  };
 
-    addIngredientHandle = type => {
-        this.props.addIngredient(type);
-        this.props.updatePurchasable();
-    }
+  addIngredientHandle = (type) => {
+    this.props.addIngredient(type);
+    this.props.updatePurchasable();
+  };
 
-    removeIngredientHandle = type => {
-        this.props.removeIngredient(type);
-        this.props.updatePurchasable();
-    }
+  removeIngredientHandle = (type) => {
+    this.props.removeIngredient(type);
+    this.props.updatePurchasable();
+  };
 
-    toggleModal = () => {
-        this.setState({
-            modalOpen: !this.state.modalOpen
-        })
-    }
+  toggleModal = () => {
+    this.setState({
+      modalOpen: !this.state.modalOpen,
+    });
+  };
 
-    handleCheckout = () => {
-        this.props.history.push("/checkout");
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="d-flex flex-md-row flex-column">
-                    <Burger ingredients={this.props.ingredients} />
-                    <Controls
-                        ingredientAdded={this.addIngredientHandle}
-                        ingredientRemoved={this.removeIngredientHandle}
-                        price={this.props.totalPrice}
-                        toggleModal={this.toggleModal}
-                        purchasable={this.props.purchasable}
-                    />
-                </div>
-                <Modal isOpen={this.state.modalOpen}>
-                    <ModalHeader>Your Order Summary</ModalHeader>
-                    <ModalBody>
-                        <h5>Total Price: {this.props.totalPrice.toFixed(0)} BDT</h5>
-                        <Summary ingredients={this.props.ingredients} />
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button style={{ backgroundColor: "#D70F64" }} onClick={this.handleCheckout}>Continue to Checkout</Button>
-                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
-                    </ModalFooter>
-                </Modal>
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div className="d-flex flex-md-row flex-column">
+          <Burger ingredients={this.props.ingredients} />
+          <Controls
+            ingredientAdded={this.addIngredientHandle}
+            ingredientRemoved={this.removeIngredientHandle}
+            price={this.props.totalPrice}
+            toggleModal={this.toggleModal}
+            purchasable={this.props.purchasable}
+          />
+        </div>
+        <Modal isOpen={this.state.modalOpen}>
+          <ModalHeader>Your Order Summary</ModalHeader>
+          <ModalBody>
+            <h5>Total Price: {this.props.totalPrice.toFixed(0)} BDT</h5>
+            <Summary ingredients={this.props.ingredients} />
+          </ModalBody>
+          <ModalFooter>
+            <Link to="/checkout">
+              <Button style={{ backgroundColor: "#D70F64" }}>
+                Continue to Checkout
+              </Button>
+            </Link>
+            <Button color="secondary" onClick={this.toggleModal}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BurgerBuilder);
--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,52 +1,54 @@
-import axios from 'axios';
-import * as actionTypes from './actionTypes';
+import axios from "axios";
+import * as actionTypes from "./actionTypes";
 
-export const addIngredient = igtype => {
-    return {
-        type: actionTypes.ADD_INGREDIENT,
-        payload: igtype,
-    }
-}
+export const addIngredient = (igtype) => {
+  return {
+    type: actionTypes.ADD_INGREDIENT,
+    payload: igtype,
+  };
+};
 
-export const removeIngredient = igtype => {
-    return {
-        type: actionTypes.REMOVE_INGREDIENT,
-        payload: igtype,
-    }
-}
+export const removeIngredient = (igtype) => {
+  return {
+    type: actionTypes.REMOVE_INGREDIENT,
+    payload: igtype,
+  };
+};
 
 export const updatePurchasable = () => {
-    return {
-        type: actionTypes.UPDATE_PURCHASABLE,
-    }
-}
+  return {
+    type: actionTypes.UPDATE_PURCHASABLE,
+  };
+};
 
 export const resetIngredients = () => {
-    return {
-        type: actionTypes.RESET_INGREDIENTS,
-    }
-}
+  return {
+    type: actionTypes.RESET_INGREDIENTS,
+  };
+};
 
-
-export const loadOrders = orders => {
-    return {
-        type: actionTypes.LOAD_ORDERS,
-        payload: orders,
-    }
-}
+export const loadOrders = (orders) => {
+  return {
+    type: actionTypes.LOAD_ORDERS,
+    payload: orders,
+  };
+};
 
 export const orderLoadFailed = () => {
-    return {
-        type: actionTypes.ORDER_LOAD_FAILED,
-    }
-}
+  return {
+    type: actionTypes.ORDER_LOAD_FAILED,
+  };
+};
 
-export const fetchOrders = () => dispatch => {
-    axios.get("https://burger-builder-4f837.firebaseio.com/orders.json")
-        .then(response => {
-            dispatch(loadOrders(response.data));
-        })
-        .catch(err => {
-            dispatch(orderLoadFailed());
-        })
-}
+export const fetchOrders = () => (dispatch) => {
+  axios
+    .get(
+      "https://burger-builder-5486f-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json"
+    )
+    .then((response) => {
+      dispatch(loadOrders(response.data));
+    })
+    .catch((err) => {
+      dispatch(orderLoadFailed());
+    });
+};
